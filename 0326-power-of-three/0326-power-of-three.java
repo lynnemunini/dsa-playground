826fcf19f7fa4b7882ee64fa68247b91a3e45097
Lynne Munini <lynnemunini65@gmail.com>
2024-03-21 12:35:21 +0300

    Time: 8 ms (90.84%), Space: 44.1 MB (30.35%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -2,12 +2,11 @@ class Solution {
     public boolean isPowerOfThree(int n) {
         if (n <= 0) return false;
         double result = n;
-        
+
         while (result > 1) {
             result = result / 3;
-            System.out.println(result);
         }
-        
+
         return result == 1;
     }
 }
